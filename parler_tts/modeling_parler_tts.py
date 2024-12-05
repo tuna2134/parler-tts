c03a6d61f5802f5e8919c4b1d9f5000cd3bd6bd7
コマリン親衛隊 <masato190411@icloud.com>
2024-12-05 21:46:46 +0900

    Update modeling_parler_tts.py

--- parler_tts/modeling_parler_tts.py
+++ parler_tts/modeling_parler_tts.py
@@ -3453,7 +3453,7 @@ class ParlerTTSForConditionalGeneration(PreTrainedModel):
         )
 
         # 6. Prepare `max_length` depending on other stopping criteria.
-        input_ids_length = input_ids.shape[-1]
+        input_ids_length = input_ids.shape[-1].item()
         has_default_max_length = kwargs.get("max_length") is None and generation_config.max_length is not None
         has_default_min_length = kwargs.get("min_length") is None and generation_config.min_length is not None
         generation_config = self._prepare_generated_length(
